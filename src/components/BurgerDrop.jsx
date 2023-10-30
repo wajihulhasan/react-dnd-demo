cdbe2f2607e59e2f955bc6d47b8c4dac9a66ea47
wajih ul hasan <m.wajeeh.qureshi@gmail.com>
2023-10-30 12:09:24 +0500

    burger ingredients dropped successfully

--- src/components/BurgerDrop.jsx
+++ src/components/BurgerDrop.jsx
@@ -2,82 +2,73 @@ import React, { useState } from 'react'
 import { useDrop } from 'react-dnd'
 import BurgerIngredient from './BurgerIngredient';
 
-const ingredients = [
-    {
-        id: 1,
-        name: 'breadTop',
-        style: {
-            display: 'block',
-            width: '30%',
-            height: '10%',
-            borderRadius: '50% 50% 0 0',
-            background: 'linear-gradient(#bc581e, #e27b36)',
-            margin: '2%',
-        },
-    },
-    {
-        id: 2,
-        name: 'bacon',
-        style: {
-            width: '30%',
-            height: '3%',
-            background: 'linear-gradient(#bf3813, #c45e38)',
-            margin: '2%',
-        },
-    },
-    {
-        id: 3,
-        name: 'cheese',
-        style: {
-            display: 'block',
-            width: '30%',
-            height: '5%',
-            borderRadius: '20px',
-            background: 'linear-gradient(#f4d004, #d6bb22)',
-            margin: '2%',
-        },
-    },
-    {
-        id: 4,
-        name: 'cheese',
-        style: {
-            display: 'block',
-            width: '30%',
-            height: '5%',
-            borderRadius: '20px',
-            background: 'linear-gradient(#f4d004, #d6bb22)',
-            margin: '2%',
-        },
-    },
-    {
-        id: 5,
-        name: 'meat',
-        style: {
-            display: 'block',
-            width: '30%',
-            height: '8%',
-            borderRadius: '20px',
-            background: 'linear-gradient(#7f3608, #702e05)',
-            margin: '2%',
-        },
-    },
-    {
-        id: 6,
-        name: 'meat',
-        style: {
-            display: 'block',
-            width: '30%',
-            height: '10%',
-            borderRadius: '0 0 30px 30px',
-            background: 'linear-gradient(#bc581e,#e27b36)',
-            margin: '2%',
-        },
-    },
-];
+
+const correctIngredientOrder = ['breadBottom', 'meat', 'cheese', 'bacon', 'breadTop'];
+
 
 const BurgerDrop = () => {
     const [board, setBoard] = useState([]);
-    const [success, setSuccess] = useState(false);
+    const [ingredients, setIngredients]=useState([
+        {
+            id: 1,
+            name: 'breadTop',
+            style: {
+                display: 'block',
+                width: '30%',
+                height: '10%',
+                borderRadius: '50% 50% 0 0',
+                background: 'linear-gradient(#bc581e, #e27b36)',
+                margin: '2%',
+            },
+        },
+        {
+            id: 2,
+            name: 'bacon',
+            style: {
+                width: '30%',
+                height: '3%',
+                background: 'linear-gradient(#bf3813, #c45e38)',
+                margin: '2%',
+            },
+        },
+    
+        {
+            id: 3,
+            name: 'cheese',
+            style: {
+                display: 'block',
+                width: '30%',
+                height: '5%',
+                borderRadius: '20px',
+                background: 'linear-gradient(#f4d004, #d6bb22)',
+                margin: '2%',
+            },
+        },
+        {
+            id: 4,
+            name: 'meat',
+            style: {
+                display: 'block',
+                width: '30%',
+                height: '8%',
+                borderRadius: '20px',
+                background: 'linear-gradient(#7f3608, #702e05)',
+                margin: '2%',
+            },
+        },
+        {
+            id: 5,
+            name: 'breadBottom',
+            style: {
+                display: 'block',
+                width: '30%',
+                height: '10%',
+                borderRadius: '0 0 30px 30px',
+                background: 'linear-gradient(#bc581e,#e27b36)',
+                margin: '2%',
+            },
+        },
+    ])
     const [{ isOver }, drop] = useDrop(() => ({
         accept: "ingredient",
         drop: (item) => addImageToBoard(item.id),
@@ -87,22 +78,31 @@ const BurgerDrop = () => {
     }));
     const addImageToBoard = (id) => {
         const ingredientToAdd = ingredients.find((ingredient) => id === ingredient.id);
-        setBoard((board) => [ingredientToAdd, ...board]); // Reverse the order
-      };
+        if(ingredientToAdd.name === correctIngredientOrder[0])
+        {
+            correctIngredientOrder.shift()
+            setBoard((board) => [ingredientToAdd ,...board]);
+            //const ingredients_left=ingredients.filter(item=>item.id!= ingredientToAdd.id)
+            setIngredients((ingredients)=>(ingredients.filter(item=>item.id!= ingredientToAdd.id)))
+            //console.log("remaining ingredients are::: ", ingredients_left);
+        }
+    };
+
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <div style={{width:'100%',height:'500px'}}>
-                {ingredients.map((ingredient) => {
+            <div style={{ width: '50%', height: '500px' }}>
+                {ingredients?.map((ingredient) => {
+                    //console.log("ingredients are :::",ingredient);
                     return <BurgerIngredient key={ingredient.id} ingredient={ingredient} id={ingredient.id} />;
                 })}
             </div>
-            <div style={{ width: '500px',height:'250px', border: '2px solid black',marginBottom:'10px'}} ref={drop}>
-                {board.map((ingredient) => {
-                    return <BurgerIngredient ingredient={ingredient} id={ingredient} />;
+            <div style={{ width: '50%', height: '500px', border: '2px solid black', marginBottom: '10px' }} ref={drop}>
+                {board.map((ingredient, index) => {
+                    return <BurgerIngredient key={ingredient.id} ingredient={ingredient} id={ingredient.id} />;
                 })}
             </div>
         </div>
-    )
+    );
 }
 
 export default BurgerDrop
